16b5e0c12a4ec8a929eb7304aef557fe0aa848f6
Daniel Almquist <danal639@student.liu.se>
2012-11-15 12:19:01 +0100

    Some changes to the content in the settings menu

--- js/privacy_settings_popup.js
+++ js/privacy_settings_popup.js
@@ -1,6 +1,26 @@
+$("#popupBillingSubmit").click(billingSubmit);
+$("#addAnotherBillingInfo").click(addAnotherBilling);
 
 function fitPopupToParrent(){
 	var sideMargins = $("#inputDiv").innerWidth()/10;
 	var topMargins = $("#inputDiv").innerHeight()/10;
-	$("#popupBody").css({"padding-left": sideMargins, "padding-top" : topMargins, "padding-right": sideMargins, "padding-bottom": topMargins});
+	$("#popupBody").css({"padding-left": sideMargins*2, "padding-top" : topMargins, "padding-right": sideMargins*2, "padding-bottom": topMargins});
+	$("#popupBillingSubmit").css({"left": $("#popupInputBox").width()/2, "top": $("#popupInputBox").height()+$("#popupBillingSubmit").height()*2});
+}
+
+function removePopup(){
+	$("#inputDiv").fadeOut(200);
+}
+
+function billingSubmit(){
+	//Check all the stuff that has been entered
+	removePopup();
+	console.log($("#popupInputBox").width()/2-$("#popupBillingSubmit").width()/2);
+}
+
+function addAnotherBilling(){
+	//Check all the stuff that has been entered
+	removePopup();
+	//Grabb all the stuff that has been entered
+	$("#inputDiv").fadeIn(400);
 }
