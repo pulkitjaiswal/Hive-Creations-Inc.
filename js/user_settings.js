3b38bbc1e225271d41d964635a7a0d64d4f13026
Daniel Almquist <danal639@student.liu.se>
2013-02-04 23:03:26 +0100

    First approach to background themes

--- js/user_settings.js
+++ js/user_settings.js
@@ -4,6 +4,7 @@
 function settingsOnLoad(){
 profileImageSettings();
 positionUserSettingsText();
+placeSettingsThemes(["img/settings/background_image_lineup.png","img/settings/background_image_lineup.png","img/settings/background_image_lineup.png","img/settings/background_image_lineup.png"]);
 }
 
 function profileImageSettings(){
@@ -28,4 +29,44 @@ function positionUserSettingsText(){
 	var textDivWidth = textDiv.width();
 	console.log($("#mainInnerWindow").css("padding-left"));
 	textDiv.css({"left": $("#mainInnerWindow").width()+parseInt($("#mainInnerWindow").css("padding-left"),10)-textDivWidth});
+}
+
+//Function for placing the chosable themes, in argument is an array with themes images to show, maximum of four themes
+function placeSettingsThemes(themesArray){
+	var previousPos = 0;
+	if(themesArray instanceof Array && typeof(themesArray[0])== "string"){
+		console.log("isArray! " + themesArray.length);
+		var i = 0;
+		var leftDiv = $("#profileSettingsThemesPredefined .profileSettingsThemesLeft");
+		var rightDiv = $("#profileSettingsThemesPredefined .profileSettingsThemesRight");
+		var rightTopText = $("<div class='hiveOrangeText'><span class='hiveBracketTextButton'>[Choose]</span> a background theme</div>").appendTo(rightDiv);
+		rightTopText.css({"position": "absolute", "top": 15});
+		console.log(leftDiv);
+		while(i<themesArray.length && i<4){
+			var div = $("<div class='profileSettingsThemesClass'></div>").appendTo(leftDiv);
+			div.attr("id", "profileSettingsThemesPredefined"+i);
+			div.css({"top": (previousPos+20)});
+			var textDiv = $("<div class='hiveOrangeText hiveBracketTextButton'></div>").appendTo(div);
+			textDiv.attr("id", "profileSettingsThemesPredefinedText"+i);
+			textDiv.css({"top": -22, "position": "absolute"});
+			textDiv.html("<span id ='profileSettingsThemesPredefinedTextViewButton" + i + "'>[view]</span> <span id ='profileSettingsThemesPredefinedTextChooseButton" + i + "'>[choose]</span>");
+			previousPos = parseInt(div.css("top"),10)+div.height();
+			//console.log(previousPos);
+			i++;
+		}
+		var rightBottomText = $("<div class='hiveOrangeText'>or</div>").appendTo(rightDiv);
+		rightBottomText.css({"position": "absolute", "top": previousPos-14});
+	}
+	var leftDiv = $("#profileSettingsThemesUser .profileSettingsThemesLeft");
+	var div = $("<div class='profileSettingsThemesClass'></div>").appendTo(leftDiv);
+	div.attr("id", "profileSettingsThemesUserDiv");
+	div.css({"top": (previousPos+45)});
+	var textDiv = $("<div class='hiveOrangeText hiveBracketTextButton'></div>").appendTo(div);
+	textDiv.attr("id", "profileSettingsThemesUserText");
+	textDiv.css({"top": -22, "position": "absolute"});
+	textDiv.html("<span id ='profileSettingsThemesUserTextViewButton'>[view]</span> <span id ='profileSettingsThemesUserTextChooseButton'>[choose]</span>");
+	var rightDiv = $("#profileSettingsThemesUser .profileSettingsThemesRight");
+	var rightBottomText = $("<div class='hiveOrangeText'></div>").appendTo(rightDiv);
+	rightBottomText.html("<span class='hiveBracketTextButton'>[Upload]</span> your own photos");
+	rightBottomText.css({"position": "absolute", "top": previousPos+40});
 }
